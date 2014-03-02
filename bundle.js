bb18b1927b8875a044bdbb34256176bfd8fb1b4a
sethvincent <sethvincent@gmail.com>
2014-03-01 18:42:55 -0800

    update bundle.js

--- bundle.js
+++ bundle.js
@@ -134,6 +134,8 @@ var Mouse = require('crtrdg-mouse');
 var Keyboard = require('crtrdg-keyboard');
 
 var Player = require('./player');
+var NPC = require('./npc');
+
 var Sprite = require('./util/sprite');
 var Camera = require('./camera');
 var Map = require('./map');
@@ -210,7 +212,41 @@ var camera = new Camera({
   viewport: { width: game.width, height: game.height },
   map: map
 });
-},{"./camera":1,"./map":3,"./player":18,"./util/sprite":20,"crtrdg-gameloop":7,"crtrdg-keyboard":10,"crtrdg-mouse":13,"imagepreloader":15}],3:[function(require,module,exports){
+
+/*
+* THE NPCs i.e. non-player characters
+* TBD Possibly an array of NPCs
+*/
+
+// Why pass in game to npc1?
+// Test creating 3 diff NPCs with each path option, horizontal, vert and static
+var npc1 = new NPC({
+  game: game,
+  map: map,
+  position: { x: 100, y: 200 },
+  path: 1
+}).addTo(game);
+
+npc1.move();
+
+var npc2 = new NPC({
+  game: game,
+  map: map,
+  position: { x: 250, y: 250 },
+  path: 0
+}).addTo(game);
+
+npc2.move();
+
+var npc3 = new NPC({
+  game: game,
+  map: map,
+  position: { x: 130, y: 300 },
+  path: 2
+}).addTo(game);
+
+npc3.move();
+},{"./camera":1,"./map":3,"./npc":18,"./player":19,"./util/sprite":21,"crtrdg-gameloop":7,"crtrdg-keyboard":10,"crtrdg-mouse":13,"imagepreloader":15}],3:[function(require,module,exports){
 var randomRGBA = require('./util/math').randomRGBA;
 
 module.exports = Map;
@@ -250,7 +286,7 @@ Map.prototype.draw = function(context, camera) {
   context.drawImage(this.image, 0, 0, this.image.width, this.image.height, -camera.position.x, -camera.position.y, this.image.width, this.image.height);
 };
 
-},{"./util/math":19}],4:[function(require,module,exports){
+},{"./util/math":20}],4:[function(require,module,exports){
 // Copyright Joyent, Inc. and other Node contributors.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
@@ -1126,6 +1162,162 @@ Tic.prototype.tick = function(dt) {
 var inherits = require('inherits');
 var Entity = require('crtrdg-entity');
 
+module.exports = NPC;
+
+function NPC(options) {
+  Entity.call(this);
+  var self = this;
+
+  this.game = options.game;
+  this.map = options.map;
+
+  this.size = {
+    x: 50,
+    y: 50
+  };
+  this.velocity = {
+    x: 0,
+    y: 0
+  };
+  this.position = options.position;
+
+  /* Set boundary width/height for NPC to walk in between */
+  this.boundary = {
+    x: this.position.x,
+    y: this.position.y,
+    width: (this.size.x * 6) + this.position.x,
+    height: (this.size.y * 7) + this.position.y
+  };
+
+  this.speed = 18;
+  this.friction = 0.2;
+  this.health = 100;
+  this.strength = 5;
+  this.color = '#ff0099';
+  this.visible = true;
+  this.points = 0;
+  this.direction = "up";
+  this.paths = ['horizontal', 'vertical', 'static'];
+
+  // Verify received valid path or default to horizontal
+  if (options.path > -1 && options.path < 3) {
+    this.path = this.paths[options.path];
+  } else {
+    this.path = 'horizontal';
+  }
+
+  this.on('update', function(interval) {
+    self.move();
+    self.velocity.x *= self.friction;
+    self.velocity.y *= self.friction;
+    self.boundaries();
+  });
+
+  this.on('draw', function(c) {
+    c.save();
+    c.fillStyle = self.color;
+    c.fillRect(
+      self.position.x,
+      self.position.y,
+      self.size.x,
+      self.size.y
+    );
+    c.restore();
+  });
+}
+
+inherits(NPC, Entity);
+
+/*
+ * move function - Kicks off moving NPC in intial direction when NPC is created.
+ * Note during game play, NPC be redirected by boundary function. e.g. hit boundary
+ */
+NPC.prototype.move = function() {
+  if (this.path === 'horizontal') {
+    if (this.direction === 'right') {
+      this.moveRight();
+    } else {
+      this.moveLeft();
+    }
+  } else if (this.path === 'vertical') {
+    if (this.direction === 'up') {
+      this.moveUp();
+    } else {
+      this.moveDown();
+    }
+  } else {
+    this.moveStop();
+  }
+
+  this.position.x += this.velocity.x * this.friction;
+  this.position.y += this.velocity.y * this.friction;
+};
+
+/* 
+ * NPC will walk between boundaries or edge of map
+ */
+NPC.prototype.boundaries = function() {
+  /* Moving Horizontal Left and went out of bounds */
+  if (this.position.x <= this.boundary.x) {
+    if (this.position.x <= 0) {
+      this.position.x = 0;
+    } else {
+      this.position.x = this.boundary.x;
+    }
+    this.moveRight();
+  }
+
+  /* Moving Horizontal Right and went out of bounds */
+  if ((this.position.x >= this.boundary.width - this.size.x) || (this.position.x >= this.map.width - this.size.x)) {
+    this.position.x = this.boundary.width - this.size.x;
+    this.moveLeft();
+  }
+
+  /* Moving Vertical Up and went out of bounds */
+  if (this.position.y <= this.boundary.y) {
+    if (this.position.y <= 0) {
+      this.position.y = 0;
+    } else {
+      this.position.y = this.boundary.y;
+    }
+    this.moveDown();
+  }
+
+  /* Moving Vertical Down and went out of bounds */
+  if ((this.position.y >= this.boundary.height - this.size.y) || (this.position.y >= this.map.height - this.size.y)) {
+    this.position.y = this.boundary.height - this.size.y;
+    this.moveUp();
+  }
+};
+
+/* Stops NPC and sets path to static i.e. stopped */
+NPC.prototype.moveStop = function() {
+  this.path = "static";
+};
+
+NPC.prototype.moveUp = function() {
+  this.velocity.y -= this.speed;
+  this.direction = "up";
+};
+
+NPC.prototype.moveDown = function() {
+  this.velocity.y += this.speed;
+  this.direction = "down";
+};
+
+NPC.prototype.moveLeft = function() {
+  this.velocity.x -= this.speed;
+  this.direction = "left";
+};
+
+NPC.prototype.moveRight = function() {
+  this.velocity.x += this.speed;
+  this.direction = "right";
+};
+},{"crtrdg-entity":5,"inherits":16}],19:[function(require,module,exports){
+var inherits = require('inherits');
+var Entity = require('crtrdg-entity');
+
 module.exports = Player;
 
 function Player(options){
@@ -1217,7 +1409,7 @@ Player.prototype.input = function(){
     this.direction = "right";
   }
 };
-},{"crtrdg-entity":5,"inherits":16}],19:[function(require,module,exports){
+},{"crtrdg-entity":5,"inherits":16}],20:[function(require,module,exports){
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -1253,7 +1445,7 @@ module.exports = {
   randomGray: randomGray,
   randomGrayAlpha: randomGray
 };
-},{}],20:[function(require,module,exports){
+},{}],21:[function(require,module,exports){
 var tic = require('tic')();
 
 module.exports = Sprite;
